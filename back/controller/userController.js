4ffa30d1d36ceb6f6208b85803c70c084d8a8fc0
Jeremy-BD <jeremy.boisnard16@gmail.com>
2023-05-24 17:40:27 +0200

    add comments and review code

--- back/controller/userController.js
+++ back/controller/userController.js
@@ -1,87 +1,106 @@
 import User from '../models/User.js';
 
-export const getAllUsers = async (req, res) => {
-  console.log(res);
-  try {
-    const users = await User.find();
-    res.json(users);
-  } catch (error) {
-    res.status(500).json({ error: 'Internal server error' });
-  }
-};
-
-export const getUserByMail = async (req, res) => {
-  const { mail } = req.params;
-
-  try {
-    const users = await User.find({ mail: { $regex: mail, $options: 'i' } });
-    res.json(users);
-  } catch (error) {
-    res.status(500).json({ error: 'Internal server error' });
-  }
-};
-
-export const getUserById = async (req, res) => {
-  const { id } = req.params;
-
-  try {
-    const user = await User.findById(id);
-    if (!user) {
-      return res.status(404).json({ error: 'User not found' });
+export default {
+  /**
+   * Récupération de tous les utilisateurs
+  */
+  getAllUsers: async (req, res) => {
+    try {
+      const users = await User.find();
+      res.json(users);
+    } catch (error) {
+      res.status(500).json({ error: 'Internal server error' });
     }
-    res.json(user);
-  } catch (error) {
-    res.status(500).json({ error: 'Internal server error' });
-  }
-};
+  },
 
-export const createUser = async (req, res) => {
-  const { name, firstname, phone_number, mail, password, role } = req.body;
+  /**
+   * Récupération de l'utilisateur selon l'id passé dans l'url
+   */
+  getUserById: async (req, res) => {
+    const { id } = req.params;
 
-  try {
-    const newUser = await User.create({
-      name,
-      firstname,
-      phone_number,
-      mail,
-      password,
-      role
-    });
-    res.status(201).json(newUser);
-  } catch (error) {
-    res.status(500).json({ error: 'Internal server error' });
-  }
-};
-
-export const updateUser = async (req, res) => {
-  const { id } = req.params;
-  const { name, firstname, phone_number, mail, password, role } = req.body;
-
-  try {
-    const updatedUser = await User.findByIdAndUpdate(
-      id,
-      { name, firstname, phone_number, mail, password, role },
-      { new: true }
-    );
-    if (!updatedUser) {
-      return res.status(404).json({ error: 'User not found' });
+    try {
+      const user = await User.findById(id);
+      if (!user) {
+        return res.status(404).json({ error: 'User not found' });
+      }
+      res.json(user);
+    } catch (error) {
+      res.status(500).json({ error: 'Internal server error' });
     }
-    res.json(updatedUser);
-  } catch (error) {
-    res.status(500).json({ error: 'Internal server error' });
-  }
-};
+  },
 
-export const deleteUser = async (req, res) => {
-  const { id } = req.params;
+  /**
+   * Récupération de l'utilisateur selon l'adresse mail passé dans l'url
+   */
+  getUserByMail: async (req, res) => {
+    const { mail } = req.params;
 
-  try {
-    const deletedUser = await User.findByIdAndDelete(id);
-    if (!deletedUser) {
-      return res.status(404).json({ error: 'User not found' });
+    try {
+      const users = await User.find({ mail: { $regex: mail, $options: 'i' } });
+      res.json(users);
+    } catch (error) {
+      res.status(500).json({ error: 'Internal server error' });
     }
-    res.json({ message: 'User deleted successfully' });
-  } catch (error) {
-    res.status(500).json({ error: 'Internal server error' });
-  }
+  },
+
+  /**
+   * Création de l'utilisateur
+   */
+  createUser: async (req, res) => {
+    const { name, firstname, phone_number, mail, password, role } = req.body;
+
+    try {
+      const newUser = await User.create({
+        name,
+        firstname,
+        phone_number,
+        mail,
+        password,
+        role
+      });
+      res.status(201).json(newUser);
+    } catch (error) {
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  },
+
+  /**
+   * Modification des données de l'utilisateur selon son id
+   */
+  updateUser: async (req, res) => {
+    const { id } = req.params;
+    const { name, firstname, phone_number, mail, password, role } = req.body;
+
+    try {
+      const updatedUser = await User.findByIdAndUpdate(
+        id,
+        { name, firstname, phone_number, mail, password, role },
+        { new: true }
+      );
+      if (!updatedUser) {
+        return res.status(404).json({ error: 'User not found' });
+      }
+      res.json(updatedUser);
+    } catch (error) {
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  },
+
+  /**
+   * Suppression de l'utilisateur selon son id
+   */
+  deleteUser: async (req, res) => {
+    const { id } = req.params;
+
+    try {
+      const deletedUser = await User.findByIdAndDelete(id);
+      if (!deletedUser) {
+        return res.status(404).json({ error: 'User not found' });
+      }
+      res.json({ message: 'User deleted successfully' });
+    } catch (error) {
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  },
 };
