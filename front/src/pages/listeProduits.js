446b800872db5adac3f6c8ce0fcbdf5d35049355
Anthony Stanix <anthony.stanix@gmail.com>
2023-05-24 16:23:35 +0200

    create first vues + header

---
+++ front/src/pages/listeProduits.js
@@ -0,0 +1,11 @@
+import Header from "../components/header";
+
+export default function Produits() {
+  return (
+    <div>
+      <Header />
+      <h1>Produits</h1>
+      <p>Produits page content</p>
+    </div>
+  );
+}
