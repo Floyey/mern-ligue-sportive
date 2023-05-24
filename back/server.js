7080cb2dba615250dbe745e563224164435a2107
Jeremy-BD <jeremy.boisnard16@gmail.com>
2023-05-24 15:47:49 +0200

    add first commit back

---
+++ back/server.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import { dbConnection } from './config/database';
+import Router from './routes/routes.js';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(dbConnection.url, dbConnection.options)
+  .then(() => {
+    console.log("connection established");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+app.use('/api', Router);
+app.get('/', (req, res) => {
+  res.send('Server is ready');
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Serve at http://localhost:${port}/api`);
+});
