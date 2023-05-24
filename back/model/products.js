3ded725d5ef97dbc48a61748359d60f58a64f6f7
Anthony Stanix <anthony.stanix@gmail.com>
2023-05-24 16:40:28 +0200

    create model product

---
+++ back/model/products.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+  id: { type: Number, required: true },
+  name: { type: String, required: true },
+  imgPrésentation: { type: String, required: true },
+  img: { type: Array, required: true },
+  year: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: String, required: true },
+  quantity: { type: String, required: true },
+});
+
+const ProductModel = mongoose.model("Produit", productSchema);
+
+export default ProductModel;
